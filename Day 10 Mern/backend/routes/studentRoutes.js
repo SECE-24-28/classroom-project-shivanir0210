29546767528f780c856e14ff38fa6e5cc234113d
shivanir0210 <shivanirasappan@gmail.com>
2025-12-17 09:54:19 +0530

    Delete Day 10 Mern/backend directory

--- Day 10 Mern/backend/routes/studentRoutes.js
+++
@@ -1,5 +0,0 @@
-const express = require('express');
-const { createStudent } = require('../controllers/studentController');
-const router = express.Router();
-router.post("/", createStudent);
-module.exports = router;
